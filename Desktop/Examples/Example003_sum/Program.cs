6423328460ecff9ce3174dbd5a81a4736a6fc947
Sveta <nostress58@ya.ru>
2022-11-28 20:39:57 +0300

    Sum A+B

---
+++ Desktop/Examples/Example003_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int Result = numberA + numberB;
+Console.WriteLine(Result);
